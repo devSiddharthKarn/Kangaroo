8d40d54601ad0731aee9bb982e5f04176991d917
devSiddharthKarn <192719686+devSiddharthKarn@users.noreply.github.com>
2026-05-30 12:02:31 +0545

    removed cache stack

--- kangaroo/src/index.ts
+++ kangaroo/src/index.ts
@@ -1,7 +1,6 @@
 import { Redis } from "ioredis"
 
 import { CacheBucket } from "./CacheBucket/cacheBucket.js";
-import { CacheStack } from "./CacheStack/cacheStack.js";
 /**
  * The main class that will be used to create cache buckets and interact with the cache, it takes a Redis connection as a parameter in the constructor example: const kang = new Kangaroo(connectionInstance)
  */
@@ -19,12 +18,6 @@ class Kangaroo {
         return new CacheBucket<TKey, TValue>(this.redisConnection,name);
     }
 
-    /*
-        * The createCacheStack function that will be used to create cache stacks, it takes a name as a parameter which will be used as a prefix for the keys in that stack, it returns an instance of the CacheStack class which can be used to interact with the cache
-    */
-    public createCacheStack<TData>(name:string):CacheStack<TData>{
-        return new CacheStack<TData>(this.redisConnection,name)
-    }
 
     /**
      * gets the original Redis instance that was passed in the constructor, this can be used to perform any Redis operations that are not covered by the CacheBucket and CacheStack classes
